d1599287c075cda4c012c21df02d2b23c8408092
Taavet Lehiste <taavet.lehiste>
2021-09-08 15:08:25 +0300

    first commit

---
+++ task1.js
@@ -0,0 +1,12 @@
+let a = 5
+b = 10
+c = a + b
+
+console.log (c)
+
+a = "5"
+d = a + b
+
+console.log (d);
+
+// tulemus 15 muutub 510-ks
---
+++ task2.js
@@ -0,0 +1,11 @@
+const Eesnimi = "Taavet"
+const paev = 13
+const kuu = 12
+const aasta = 1997
+const Hobid = ["Videomangud", "Söömine"];
+const about = { Eesnimi,paev,kuu,aasta,Hobid }
+
+
+console.log (about)
+
+console.log (paev+"."+kuu+"."+aasta)
